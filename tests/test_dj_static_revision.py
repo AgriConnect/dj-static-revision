066ded38edbd370b48620c64635ed9998f8f040a
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2019-02-19 10:36:44 +0700

    First working version.

---
+++ tests/test_dj_static_revision.py
@@ -0,0 +1,63 @@
+import shutil
+from pathlib import Path
+
+from dulwich.repo import Repo
+from dulwich.errors import NotGitRepository
+from dj_static_revision.utils import get_source_revision
+
+
+TEST_DATA = 'aabbcc'
+
+
+class TestGetSourceRevision:
+    def setup(self):
+        folder = Path.cwd()
+        filepath = folder / 'manage.py'
+        filepath.touch()
+        self.original_revision = None
+
+    def test_normal_from_file(self):
+        folder = Path.cwd()
+        filepath = folder / '.version'
+        filepath.write_text(TEST_DATA)
+        revision = get_source_revision()
+        assert revision == TEST_DATA
+
+    def test_normal_from_repo(self):
+        # Create repo
+        folder = Path.cwd()
+        try:
+            rp = Repo(str(folder))
+        except NotGitRepository:
+            rp = Repo.init(str(folder))
+        try:
+            version = rp.head().decode()
+            self.original_revision = version
+        except KeyError:
+            FILE_NAME_TEST = 'file_test.txt'
+            test_file = folder / FILE_NAME_TEST
+            test_file.touch()
+            rp.stage(FILE_NAME_TEST.encode())
+            version = rp.do_commit(b'Test commit').decode()
+        v = get_source_revision()
+        assert v == version[:7]
+
+    def teardown(self):
+        folder = Path.cwd()
+        try:
+            (folder / 'manage.py').unlink()
+        except FileNotFoundError:
+            pass
+        try:
+            (folder / '.version').unlink()
+        except FileNotFoundError:
+            pass
+        if self.original_revision is None:
+            # Remove the Git repo created in test
+            try:
+                rp = Repo(str(folder))
+                head = rp[rp.head()]
+                if not len(head.parents):
+                    shutil.rmtree(folder / '.git')
+            except NotGitRepository:
+                pass
